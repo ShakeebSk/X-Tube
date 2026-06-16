1c57aad21a00429e25335f9974d2cacead1e90e8
Shakeeb Shaikh <eren.85yeager@gmail.com>
2026-06-16 10:19:04 +0530

    Set up FastAPI with CORS and routing

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,33 @@
-print("hii")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from backend.db.base import Base
+from db.base import Base
+from routes import auth,upload,video,video_interactions
+from db.db import engine
+
+
+app = FastAPI()
+
+origins = ["http://localhost", "http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router,prefix="/auth")
+app.include_router(upload.router,prefix="/upload/video")
+app.include_router(video.router,prefix="/videos")
+app.include_router(video_interactions.router,prefix="/media")
+
+
+
+@app.get("/")
+def root():
+    return "Hello, World!!!!"
+
+
+Base.metadata.create_all(engine)
